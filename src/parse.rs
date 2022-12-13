96e04c8f0a87ebbea2dcd2bcefbb6292b3f12866
Matt Hunzinger <matthunz2@gmail.com>
2022-12-12 20:24:16 -0500

    Create Parser struct

--- src/parse.rs
+++ src/parse.rs
@@ -1,88 +1,38 @@
-use crate::{
-    midi::Octave,
-    render::{Chord, Duration, Note, Renderer},
-    Natural,
-};
+use std::str::{Lines, SplitAsciiWhitespace};
 
-pub fn parse<'a>(renderer: &'a Renderer, input: &str) -> Vec<Chord<'a>> {
-    let mut chars = input.chars().peekable();
-    let mut duration = Duration::Quarter;
-
-    let mut chords = Vec::new();
-
-    while let Some(c) = chars.next() {
-        match c {
-            '\\' => todo!(),
-            c => {
-                let natural = Natural::try_from(c).unwrap();
-
-                let mut i = 0;
-                match chars.peek() {
-                    Some('\'') => {
-                        chars.next();
-                        i = 1;
-
-                        while chars.peek().copied() == Some('\'') {
-                            chars.next();
-                            i += 1;
-                        }
-                    }
-                    Some(',') => {
-                        chars.next();
-                        i = -1;
-
-                        while chars.peek().copied() == Some('\'') {
-                            chars.next();
-                            i -= 1;
-                        }
-                    }
-                    _ => {}
-                }
-
-                if let Some(c) = chars.peek() {
-                    if let Some(n) = c.to_digit(10) {
-                        chars.next();
-                        duration = match n {
-                            4 => Duration::Quarter,
-                            _ => todo!(),
-                        };
-                    }
-                }
-
-                match chars.next() {
-                    Some(' ') | None => {}
-                    Some(_) => todo!(),
-                }
-
-                // TODO check octave
-                let note = Note::new(natural, Octave::new_unchecked(i + 3), None);
-                chords.push(Chord::new(&[note], duration, &renderer));
-            }
-        }
-    }
-
-    chords
+pub enum Item<'a> {
+    Command {
+        name: &'a str,
+        args: SplitAsciiWhitespace<'a>,
+    },
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::{
-        render::{measure::Measure, KeySignature, Renderer},
-        Key, Pitch,
-    };
+pub struct Parser<'a> {
+    lines: Lines<'a>,
+}
 
-    use super::parse;
+impl<'a> Parser<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self {
+            lines: input.lines(),
+        }
+    }
+}
 
-    #[test]
-    fn f() {
-        let s = "c'4 e' g' c''";
+impl<'a> Iterator for Parser<'a> {
+    type Item = Item<'a>;
 
-        let renderer = Renderer::default();
-        let chords = parse(&renderer, s);
-        let key_signature = KeySignature::new(Key::major(Pitch::G), &renderer);
-        let measure = Measure::new(chords, Some(key_signature));
-        let svg = renderer.render(&measure);
-
-        svg::save("ly.svg", &svg).unwrap();
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(line) = self.lines.next() {
+            if let Some(tail) = line.strip_prefix('\\') {
+                let mut args = tail.split_ascii_whitespace();
+                let name = args.next().unwrap();
+                Some(Item::Command { name, args })
+            } else {
+                todo!()
+            }
+        } else {
+            None
+        }
     }
 }
